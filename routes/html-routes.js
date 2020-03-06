03217db2936a9f1067cba2a2b6236ee79f6c9a88
Hyuk Jun Kwon <hyukjunkwon91@gmail.com>
2020-03-06 11:29:22 -0800

    unfinished routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,7 +1,15 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-  res.render('home');
+  res.render('shop');
+});
+
+router.get('/team', (req, res) => {
+  res.render('team');
+});
+
+router.get('/carts', (req, res) => {
+  res.render('carts');
 });
 
 module.exports = router;
